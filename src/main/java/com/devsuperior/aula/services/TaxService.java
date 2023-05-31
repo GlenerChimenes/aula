0b44f7e9e1e14666d7197066bc74e06a1e623730
GlenerChimenes <glener.chimenes@gmail.com>
2023-05-31 10:25:05 -0300

    projeto aula

---
+++ src/main/java/com/devsuperior/aula/services/TaxService.java
@@ -0,0 +1,14 @@
+package com.devsuperior.aula.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaxService {
+
+	
+	public double tax(double amount) {
+
+		return amount * 0.2;
+	}
+
+}
